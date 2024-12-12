5a06ec72d3713b991872f177d523ab333f19af1d
Ehsan Akhtar <ehsanakhtar123@gmail.com>
2024-12-13 04:51:51 +0530

    Initial commit: Flutter API project

---
+++ lib/home_screen.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+class _HomeScreenState extends State<HomeScreen> {
+  List posts = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
+  Future<void> fetchPosts() async {
+    try {
+      final response = await http
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+      if (response.statusCode == 200) {
+        setState(() {
+          posts = json.decode(response.body);
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load the posts ');
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isLoading
+          ? const CircularProgressIndicator()
+          : ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(posts[index]['title']),
+                  subtitle: Text(posts[index]['body']),
+                );
+              }),
+    );
+  }
+}
